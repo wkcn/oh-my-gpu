180c0d133eca5edb81beebe08565c218a7d6fb24
wkcn <wkcn@live.cn>
2017-12-21 06:28:04 +0000

    multi server

--- client.py
+++ client.py
@@ -2,18 +2,13 @@ import json
 import requests
 import config
 
-PORT = config.PORT 
-SERVER_IP = config.SERVER_IP
-
-SERVER_ADDRESS = "http://{}:{}".format(SERVER_IP, PORT) 
-print (SERVER_ADDRESS)
-
-QUERY_ADDR = SERVER_ADDRESS + '/query'
-
 data = {"opcode" : 0}
+bjson = json.dumps(data).encode()
 headers = {'Content-Type': 'application/json'}
-req = requests.post(QUERY_ADDR, data = json.dumps(data).encode(), headers = headers)
-js_recv = json.loads(req.json())
-state, process = js_recv
 
-print (state[0]["memory.total"])
+for addr in config.SERVER_ADDRS:
+    QUERY_ADDR = 'http://%s/query' % addr
+    req = requests.post(QUERY_ADDR, data = bjson, headers = headers)
+    js_recv = json.loads(req.json())
+    state, process = js_recv
+    print (state[0]["memory.total"])
