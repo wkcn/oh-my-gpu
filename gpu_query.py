ffdd3e6f66f35a1accdb42faa49b024baf931c31
wkcn <wkcn@live.cn>
2017-12-29 15:09:39 +0800

    improve the performance of ps

--- gpu_query.py
+++ gpu_query.py
@@ -8,17 +8,18 @@ def get_gpu_info():
     for g in [nv]:
         state.extend(g.get_gpu_state())
         process.extend(g.get_gpu_process())
-
     uuid2gid = dict() # uuid -> gid
+    
     for i, g in enumerate(state):
         uuid2gid[g["uuid"]] = i
         g["gpu_id"] = i
-
+        
     for i, p in enumerate(process):
         uuid = p["uuid"]
         gid = uuid2gid[uuid]
         process[i]["gpu_id"] = gid
-
+        
+    ps.update_info()
     for i, p in enumerate(process):
         pid = p["pid"]
         username, pname = ps.get_username_from_pid(pid) 
--- utils/ps.py
+++ utils/ps.py
@@ -18,7 +18,7 @@ def get_loc_value(loc_str, pattern, context):
         left -= 1
     while right + 1 < len(context) and context[right + 1] != ' ':
         right += 1
-    return context[left:right + 1].strip() 
+    return context[left:right + 1].strip()  
 
 def get_loc_value2(loc_str, context):
     '''
@@ -35,21 +35,72 @@ def get_loc_value2(loc_str, context):
     while right + 1 < len(ep) and ep[right + 1] == '=':
         right += 1
     return context[2][left:right + 1].strip()
+    
+def get_column2(loc_str, context):
+    '''
+    Name
+    ========
+    value
+    '''
+    loc_i = context[0].find(loc_str)
+    
+    ep = context[1]
+    left = loc_i
+    right = loc_i
+    
+    while left > 0 and ep[left - 1] == '=':
+        left -= 1
+    while right + 1 < len(ep) and ep[right + 1] == '=':
+        right += 1
+    return [context[i][left:right + 1].strip() for i in range(2, len(context))]
 
+
+
+
+def get_task_list():
+    cmd = 'tasklist'
+    lines = os.popen(cmd).readlines()
+    
+    pnames = get_column2("Image Name", lines[1:])
+    pids = get_column2("PID", lines[1:])
+    sids = get_column2("Session#", lines[1:])
+    res = dict()
+    for pid, pname, sid in zip(pids, pnames, sids):
+        res[int(pid)] = dict(pname = pname, sid = int(sid) )
+    return res
+
+
+def get_session_list():
+    cmd = 'query session'
+    lines = os.popen(cmd).readlines()
+    pattern = lines[0]
+    res = dict()
+    for i in range(1, len(lines)):
+        s = lines[i]
+        sid = get_loc_value("ID", pattern, s)
+        uname = get_loc_value("USERNAME", pattern, s)
+        res[int(sid)] = uname
+    return res
+        
+TASK_LIST = dict() # PID -> INFO   
+SESSION_LIST = dict() # SID -> NAME    
+def update_info():
+    if LINUX:
+        return
+    # For windows
+    global TASK_LIST
+    global SESSION_LIST
+    TASK_LIST = get_task_list()
+    SESSION_LIST = get_session_list()
 
 def get_username_from_pid(pid):
     if LINUX:
         p = psutil.Process(pid)
         return p.username(), p.cwd() + "|" + p.name()
-    cmd = 'tasklist /FI "PID eq %d"' % pid
-    lines = os.popen(cmd).readlines()
-            
-    pname = get_loc_value2("Image Name", lines[1:])
-    sid = int(get_loc_value2("Session#", lines[1:]))
-                    
-    cmd = 'query session %d' % sid
-    lines = os.popen(cmd).readlines()
-    username = get_loc_value("USERNAME", lines[0], lines[1])
+    pinfo = TASK_LIST[pid]        
+    pname = pinfo["pname"]
+    sid = pinfo["sid"]
+    username = SESSION_LIST[sid]
     return username, pname
 
 if __name__ == "__main__":
